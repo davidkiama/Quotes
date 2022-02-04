697d81b9cbb9e182ae6e885cff39de70d528679e
davidkiama <davidkiama98@gmail.com>
2022-02-04 14:07:15 +0300

    Added the quote fuctionality to its own component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,26 +6,4 @@ import { Quote } from './quote';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  quotes: Quote[] = [
-    {
-      id: 1,
-      author: 'Kate',
-      quote: "I am going to stand outside.If anyone asks I'm outstanding ",
-      date: 'Feb 2 2022',
-    },
-    {
-      id: 2,
-      author: 'Shigeta',
-      quote:
-        "Just because you're trash doesn't mean you cant do great things. It's called garbage can not garbage cannot",
-      date: 'Feb 2 2022',
-    },
-    {
-      id: 3,
-      author: 'Beth',
-      quote: 'Anything possible when you sound caucasian on the phone',
-      date: 'Feb 2 2022',
-    },
-  ];
-}
+export class AppComponent {}
---
+++ src/app/quote/quote.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+import { Quote } from '../quote';
+
+@Component({
+  selector: 'app-quote',
+  templateUrl: './quote.component.html',
+  styleUrls: ['./quote.component.css'],
+})
+export class QuoteComponent implements OnInit {
+  quotes: Quote[] = [
+    {
+      id: 1,
+      author: 'Kate',
+      quote: "I am going to stand outside.If anyone asks I'm outstanding ",
+      date: 'Feb 2 2022',
+    },
+    {
+      id: 2,
+      author: 'Shigeta',
+      quote:
+        "Just because you're trash doesn't mean you cant do great things. It's called garbage can not garbage cannot",
+      date: 'Feb 2 2022',
+    },
+    {
+      id: 3,
+      author: 'Beth',
+      quote: 'Anything possible when you sound caucasian on the phone',
+      date: 'Feb 2 2022',
+    },
+  ];
+
+  constructor() {}
+
+  ngOnInit(): void {}
+}
