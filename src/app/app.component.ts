4673c70b756e5d53ae8f2c2e657c09b1efbcf392
davidkiama <davidkiama98@gmail.com>
2022-02-04 13:59:06 +0300

    Added the class component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
 import { Component } from '@angular/core';
+import { Quote } from './quote';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'quotes';
+  quotes: Quote[] = [
+    {
+      id: 1,
+      author: 'Kate',
+      quote: "I am going to stand outside.If anyone asks I'm outstanding ",
+      date: 'Feb 2 2022',
+    },
+    {
+      id: 2,
+      author: 'Shigeta',
+      quote:
+        "Just because you're trash doesn't mean you cant do great things. It's called garbage can not garbage cannot",
+      date: 'Feb 2 2022',
+    },
+    {
+      id: 3,
+      author: 'Beth',
+      quote: 'Anything possible when you sound caucasian on the phone',
+      date: 'Feb 2 2022',
+    },
+  ];
 }
