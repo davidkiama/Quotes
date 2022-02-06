3bcbe201e2d3717379235dca9d39b76384b0cbb0
davidkiama <davidkiama98@gmail.com>
2022-02-06 14:12:41 +0300

    Added fuctionality to display time passed

--- src/app/time-passed.pipe.ts
+++ src/app/time-passed.pipe.ts
@@ -6,8 +6,9 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class TimePassedPipe implements PipeTransform {
   transform(value: any): string {
     let today: any = new Date();
+    let dayCreated: any = new Date(value);
 
-    let days = (today - value) / (1000 * 3600 * 24);
+    let days = (today - dayCreated) / (1000 * 3600 * 24);
 
     if (days < 1) {
       return `${Math.round(days * 24)}h`;
